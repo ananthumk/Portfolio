0a49de32d4f7bb94ef29bf5d1a3184175ddd7181
Ananthu <ananthusmk@gmail.com>
2025-08-16 19:45:20 +0530

    commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,6 +13,9 @@ export const Contact = () => {
   const [searching, setSearching] = useState(false)
   const formRef = useRef()
 
+  const githubLink = import.meta.env.VITE_githubLink
+  const linkedlnLink = import.meta.env.VITE_linkedln
+
   const icon = searching ? <AiOutlineLoading3Quarters size={20} /> : <FiSend size={20} />
   const buttonText = !searching ? 'Send Message' : 'Sending...'
 
@@ -77,12 +80,12 @@ export const Contact = () => {
                 </div>
                 <h3 className="text-xl md:text-2xl p-2 font-semibold">Follow Me</h3>
                 <div className="flex items-center gap-4 p-1">
-                    <button className="group  cursor-pointer hover:scale-y-105 bg-neutral-300/10 rounded-xl p-2 md:p-3">
+                    <a href={linkedlnLink}><button className="group  cursor-pointer hover:scale-y-105 bg-neutral-300/10 rounded-xl p-2 md:p-3">
                        <FaLinkedinIn size={24} className='text-white transition-transform duration-300 group-hover:scale-y-105' />
-                    </button>
-                    <button className="group cursor-pointer hover:scale-y-105 bg-neutral-300/10 rounded-xl p-2 md:p-3">
+                    </button></a>
+                    <a href={githubLink}><button className="group cursor-pointer hover:scale-y-105 bg-neutral-300/10 rounded-xl p-2 md:p-3">
                        <FiGithub size={24} className='text-white  transition-transform duration-300 group-hover:scale-y-105' />
-                    </button>
+                    </button></a>
                 </div>
             </div>
             {/* LEFT SIDE */}
